59d92fdf9980fb0034c1005d70105bfd7a184b3e
mattrossman <rossman@gatech.edu>
2021-03-18 11:53:23 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+function App() {
+  return <div tw="font-bold">hellow</div>
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import twindJsx from '@twind/vite-plugin-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh(), twindJsx()],
+})
